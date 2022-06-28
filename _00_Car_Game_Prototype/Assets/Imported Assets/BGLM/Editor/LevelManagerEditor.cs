68f380c775e24eaec29407d1d727e841adcd98ad
Nikita Kleimenov <nikita.kleimenov@nure.ua>
2022-06-28 19:01:11 +0300

    Add a model pack

---
+++ _00_Car_Game_Prototype/Assets/Imported Assets/BGLM/Editor/LevelManagerEditor.cs
@@ -0,0 +1,96 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(LevelManager))]
+public class LevelManagerEditor : Editor
+{
+    private SerializedProperty _editorMode;
+    private LevelManager _levelManager;
+    private ReorderableList listLvl;
+
+    private void Awake()
+    {
+        _levelManager = target as LevelManager;
+    }
+
+    private void OnEnable()
+    {
+        _editorMode = serializedObject.FindProperty("editorMode");
+        listLvl = new ReorderableList(serializedObject, serializedObject.FindProperty("Levels"), true, true, true, true);
+        listLvl.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            var element = listLvl.serializedProperty.GetArrayElementAtIndex(index);
+
+            EditorGUI.IntField(new Rect(rect.x, rect.y, 30, EditorGUIUtility.singleLineHeight), index + 1);
+
+            if (GUI.Button(new Rect(rect.x + 36, rect.y, 50, EditorGUIUtility.singleLineHeight), new GUIContent("Select")))
+            {
+                serializedObject.FindProperty("CurrentLevelIndex").intValue = index;
+                serializedObject.ApplyModifiedProperties();
+                _levelManager.SelectLevel(index);
+            }
+
+            EditorGUI.PropertyField(
+                new Rect(rect.x + 90, rect.y, 200, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("LevelPrefab"), GUIContent.none);
+
+
+            if (GUI.Button(new Rect(rect.x + 300, rect.y, 50, EditorGUIUtility.singleLineHeight), new GUIContent("Clear")))
+            {
+                _levelManager.ClearListAtIndex(index);
+            }
+
+            if (GUI.Button(new Rect(rect.x + 347, rect.y, 50, EditorGUIUtility.singleLineHeight), new GUIContent("Delete")))
+            {
+                _levelManager.Levels.RemoveAt(index);
+            }
+        };
+
+        listLvl.drawHeaderCallback = (Rect rect) => {
+            EditorGUI.LabelField(rect, "Levels");
+        };
+    }
+
+    public override void OnInspectorGUI()
+    {
+        _editorMode.boolValue = GUILayout.Toggle(_editorMode.boolValue, new GUIContent("Editor Mode"), GUILayout.Width(100), GUILayout.Height(20));
+        serializedObject.ApplyModifiedProperties();
+        _levelManager.editorMode = _editorMode.boolValue;
+        if (_editorMode.boolValue) DrawSelectedLevel();
+
+        serializedObject.Update();
+        listLvl.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+
+        if (GUILayout.Button("Clear Player Prefs", GUILayout.Width(200), GUILayout.Height(20)))
+            PlayerPrefs.DeleteAll();
+    }
+
+    private void DrawSelectedLevel()
+    {
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUI.BeginChangeCheck();
+        int index = EditorGUILayout.IntField("Current Level", _levelManager.CurrentLevelIndex + 1);
+        if (EditorGUI.EndChangeCheck())
+        {
+            _levelManager.SelectLevel(index - 1);
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        if (GUILayout.Button("<<", GUILayout.Width(30), GUILayout.Height(20)))
+        {
+            _levelManager.PrevLevel();
+        }
+        if (GUILayout.Button(">>", GUILayout.Width(30), GUILayout.Height(20)))
+        {
+            _levelManager.NextLevel();
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+}
---
+++ _00_Car_Game_Prototype/Assets/Imported Assets/BGLM/LevelManager.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System;
+
+public class LevelManager : MonoBehaviour
+{
+    #region Singletone
+    private static LevelManager _default;
+    public static LevelManager Default { get => _default; }
+    public LevelManager() => _default = this;
+    #endregion
+
+    const string PREFS_KEY_LEVEL_ID = "CurrentLevelCount";
+    const string PREFS_KEY_LAST_INDEX = "LastLevelIndex";
+
+    public bool editorMode = false;
+    public int CurrentLevelCount => PlayerPrefs.GetInt(PREFS_KEY_LEVEL_ID, 0) + 1;
+    public int CurrentLevelIndex;
+    public List<Level> Levels = new List<Level>();
+    public Action OnLevelStarted;
+
+
+    public void Awake()
+    {
+#if !UNITY_EDITOR
+        editorMode = false;
+#endif
+        if (!editorMode) SelectLevel(PlayerPrefs.GetInt("LastLevelIndex"), true);
+    }
+
+    private void OnDestroy()
+    {
+        PlayerPrefs.SetInt(PREFS_KEY_LAST_INDEX, CurrentLevelIndex);
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("LastLevelIndex", CurrentLevelIndex);
+    }
+
+
+    public void StartLevel()
+    {
+        //SendStart();
+        OnLevelStarted?.Invoke();
+    }
+
+    public void RestartLevel()
+    {
+        //SendRestart();
+        SelectLevel(CurrentLevelIndex, false);
+    }
+
+    public void NextLevel()
+    {
+        //SendComplete();
+        if (!editorMode)
+            PlayerPrefs.SetInt(PREFS_KEY_LEVEL_ID, (PlayerPrefs.GetInt(PREFS_KEY_LEVEL_ID) + 1));
+        SelectLevel(CurrentLevelIndex + 1);
+    }
+
+    public void ClearListAtIndex(int levelIndex)
+    {
+        Levels[levelIndex].LevelPrefab = null;
+    }
+
+    public void SelectLevel(int levelIndex, bool indexCheck = true)
+    {
+        if (indexCheck)
+            levelIndex = GetCorrectedIndex(levelIndex);
+
+        if (Levels[levelIndex].LevelPrefab == null)
+        {
+            Debug.Log("<color=red>There is no prefab attached!</color>");
+            return;
+        }
+
+        var level = Levels[levelIndex];
+
+        if (level.LevelPrefab)
+        {
+            SelLevelParams(level);
+            CurrentLevelIndex = levelIndex;
+        }
+    }
+
+    public void PrevLevel() =>
+        SelectLevel(CurrentLevelIndex - 1);
+
+    private int GetCorrectedIndex(int levelIndex)
+    {
+        if (editorMode)
+            return levelIndex > Levels.Count - 1 || levelIndex <= 0 ? 0 : levelIndex;
+        else
+        {
+            int levelId = PlayerPrefs.GetInt(PREFS_KEY_LEVEL_ID);
+            if (levelId > Levels.Count - 1)
+            {
+                if (Levels.Count > 1)
+                {
+                    while (true)
+                    {
+                        levelId = UnityEngine.Random.Range(0, Levels.Count);
+                        if (levelId != CurrentLevelIndex) return levelId;
+                    }
+                }
+                else return UnityEngine.Random.Range(0, Levels.Count);
+            }
+            return levelId;
+        }
+    }
+
+    private void SelLevelParams(Level level)
+    {
+        if (level.LevelPrefab)
+        {
+            ClearChilds();
+#if UNITY_EDITOR
+            if (Application.isPlaying)
+            {
+                Instantiate(level.LevelPrefab, transform);
+            }
+            else PrefabUtility.InstantiatePrefab(level.LevelPrefab, transform);
+            foreach (IEditorModeSpawn child in GetComponentsInChildren<IEditorModeSpawn>())
+                child.EditorModeSpawn();
+#else
+            Instantiate(level.LevelPrefab, transform);
+#endif
+        }
+
+        if (level.SkyboxMaterial)
+        {
+            RenderSettings.skybox = level.SkyboxMaterial;
+        }
+    }
+
+    private void ClearChilds()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject destroyObject = transform.GetChild(i).gameObject;
+            DestroyImmediate(destroyObject);
+        }
+    }
+
+
+
+    #region Analitics Events
+
+/*    public void SendStart()
+    {
+        string content = (PlayerPrefs.GetInt(PREFS_KEY_LEVEL_ID, 0) + 1).ToString();
+    }
+
+    public void SendRestart()
+    {
+        string content = (PlayerPrefs.GetInt(PREFS_KEY_LEVEL_ID, 0) + 1).ToString();
+    }
+
+    public void SendComplete()
+    {
+        string content = (PlayerPrefs.GetInt(PREFS_KEY_LEVEL_ID, 0) + 1).ToString();
+    }*/
+
+    #endregion
+}
+
+[System.Serializable]
+public class Level
+{
+    public GameObject LevelPrefab;
+    public Material SkyboxMaterial;
+}
